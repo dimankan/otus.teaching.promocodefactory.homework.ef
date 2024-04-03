e33957514b1e125dd67326a9b5e47d86130e6841
dimankan <asus>
2024-04-03 13:32:47 +0300

    1. Добавить Entity Framework Core в DataAccess проект и сделать реализацию IRepository в виде EfRepository, которая будет работать с DataContext Entity Framework.

---
+++ src/Otus.Teaching.PromoCodeFactory.DataAccess/DataBaseContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Otus.Teaching.PromoCodeFactory.DataAccess
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ src/Otus.Teaching.PromoCodeFactory.DataAccess/Repositories/EfRepository.cs
@@ -0,0 +1,22 @@
+﻿using Otus.Teaching.PromoCodeFactory.Core.Abstractions.Repositories;
+using Otus.Teaching.PromoCodeFactory.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Otus.Teaching.PromoCodeFactory.DataAccess.Repositories
+{
+    public class EfRepository<T> : IRepository<T>
+        where T : BaseEntity
+    {
+        public Task<IEnumerable<T>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
